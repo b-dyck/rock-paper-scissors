02d4f22d71878db8cd70777dbe36c0d9fbea48d0
Bryce Dyck <bryce.dyck@alumni.ubc.ca>
2022-05-06 08:48:01 -0700

    Whiteboard problem and functions needed

---
+++ javascript.js
@@ -0,0 +1,30 @@
+/* Whiteboarding
+
+computerPlay ()
+    Choose rock paper or scissors
+    return rock paper or scissors
+
+
+playerSelection ()
+    returm lowercase(prompt(choose option))
+
+
+singleRound (playerSelection, computerPlay())
+    if same tie
+    if different choose winner
+    return result
+
+
+game()
+    let playerWins = 0
+    let computerWins = 0
+    for (1 to 5)
+        if singleRound() == player
+            playerWins++
+        else 
+            computerWins++
+    print playerWins + computerWins
+
+*/
+
+
